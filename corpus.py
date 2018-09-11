9198a8c4a3b73d1666d189baaf4061f9dea28f7c
Emiliano Kokic <emikokic@gmail.com>
2018-09-11 00:20:16 -0300

    Renaming, bug fixing and improvements

--- corpus.py
+++ corpus.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def files(path):
     '''Gets the filenames from a directory'''
-
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
@@ -13,7 +12,6 @@ def files(path):
 
 def spread_artID(df):
     '''Associates each sentence with its corresponding article ID'''
-
     artID_list = df['art_ID'].tolist()
     artID = 0
     for idx, elem in enumerate(artID_list):
@@ -27,7 +25,6 @@ def spread_artID(df):
 
 
 def get_word(idx, word_mapping):
-
     return word_mapping.iloc[idx, word_mapping.columns.get_loc('word')]
 
 
@@ -39,8 +36,7 @@ def create_docs_df(dir_path, out_path):
 
     docs = pd.DataFrame(columns=['sentence','art_ID'])
     numOfSentences = 0
-
-    count = 0
+    count = 0 # Used for the 'progress bar'
 
     for file in files(dir_path):
         filepath = dir_path + file
@@ -55,14 +51,14 @@ def create_docs_df(dir_path, out_path):
         # Mapping
         doc['sentence'] = doc['sentence'].map(lambda sen: [get_word(idx, word_mapping) for idx in sen])
         numOfSentences += doc.shape[0]
-        pd.to_pickle(doc, df_path)
-
         if count % 10 == 0:
             print(count, end=' ')
         count += 1
-
+        
+        pd.to_pickle(doc, df_path)
     print('Cantidad de oraciones: {}'.format(numOfSentences))
 
+    
 def get_tag(x):
     tags = ['PER', 'LOC', 'ORG', 'MISC']
     return tags[x]    
@@ -73,7 +69,6 @@ def create_coarseNE_df(dir_path, out_path):
     numOfEntities = 0
     count = 0
     for file in files(dir_path):
-
         filepath = dir_path + file
         df_path = out_path + 'coarseNE_' + file
 
@@ -90,20 +85,20 @@ def create_coarseNE_df(dir_path, out_path):
         coarseNE = coarseNE.drop(columns='named-entity')
         
         numOfEntities += coarseNE.shape[0]
-        pd.to_pickle(coarseNE, df_path)
-
         if count % 10 == 0:
             print(count, end=' ')
-        count += 1    
-    
+        count += 1
+        
+        pd.to_pickle(coarseNE, df_path)    
     print('Cantidad de entidades: {}'.format(numOfEntities))
+
     
 def main():
-#     start = time.time()
+    start = time.time()
 #     create_docs_df('./corpus_WiNER/Documents/', './corpus_WiNER/docs_df/')
 #     create_coarseNE_df('./corpus_WiNER/CoarseNE/', './corpus_WiNER/coarseNE_df/')
-#     end = time.time()
-#     print('Demora: {}'.format(end - start))
+    end = time.time()
+    print('Demora: {}'.format(end - start))
 
 
 if __name__ == '__main__':
--- embeddings.py
+++ embeddings.py
@@ -1,102 +1,105 @@
 import numpy as np
 
-def fill_w2vMatrix(sen_matrix, w2v_model)
+def fill_w2vMatrix(sentence, sen_matrix, D, w2v_model):
     '''
     @return (sen_matrix, count) tuple where
     sen_matrix: filled matrix after apply w2v_model.
     count: amount of words that are not in the vocabulary.
     '''
     count = 0
+    no_vocab_list = []
     for idx, word in enumerate(sentence):
         try:
-            vector = model.get_vector(word)
+            vector = w2v_model.get_vector(word)
         except KeyError: # the word is not in the vocabulary
             vector = np.zeros((D))
             count += 1
+            no_vocab_list.append(word)
         sen_matrix[:,idx] = vector
-    return sen_matrix, count
+    return sen_matrix, count, no_vocab_list
+
+
+# def concat_vectors(sentence, D, W, w2v_model):
+#     '''
+#     sentence: list of strings
+#     D: dimensionality of word vectors
+#     W: window
+#     w2v_model: word2vec model
+    
+#     return vectors: L x 2WD matrix, where L is the length of the sentence.
+#     i.e. each row contains a word vector after apply concatenation strategy.
+#     '''
+#     L = len(sentence)
+#     shape = (D, L)
+#     sen_matrix = np.zeros(shape)
+#     vectors = np.zeros((L, 2*W*D))
+
+#     sen_matrix, count = fill_w2vMatrix(sentence, sen_matrix, D, w2v_model)
+    
+#     # I: index of the target word
+#     for I in range(0, L):             
+#         concat_vec = np.empty(0)      
+#         # Padding with vector of zeros on the left
+#         if I - W < 0:
+#             concat_vec = np.append(concat_vec, np.zeros(abs(I-W) * D))
+#         # Concat vectors from the sentence
+#         for i in range(I - W, I + W + 1):             
+#             if i >= 0 and i != I and i < L:
+#                 concat_vec = np.append(concat_vec, sen_matrix[:, i])          
+#         # Padding with vector of zeros on the right
+#         if I + W >= L:
+#             concat_vec = np.append(concat_vec, np.zeros(abs(I+W+1-L) * D))
+
+#         vectors[I,:] = concat_vec
         
-def concat_vectors(sentence, D, W, w2v_model):
-    '''
-    sentence: list of strings
-    D: dimensionality of word vectors
-    W: window
-    w2v_model: word2vec model
-    
-    return vectors: L x 2WD matrix, where L is the length of the sentence.
-    i.e. each row contains a word vector after apply concatenation strategy.
-    '''
-    L = len(sentence)
-    shape = (D, L)
-    sen_matrix = np.zeros(shape)
-    vectors = np.zeros((L, 2*W*D))
-
-    sen_matrix = fill_w2vMatrix(sen_matrix, w2v_model)
-    
-    # I: index of the target word
-    for I in range(0, L):             
-        concat_vec = np.empty(0)      
-        # Padding with vector of zeros on the left
-        if I - W < 0:
-            concat_vec = np.append(concat_vec, np.zeros(abs(I-W) * D))
-        # Concat vectors from the sentence
-        for i in range(I - W, I + W + 1):             
-            if i >= 0 and i != I and i < L:
-                concat_vec = np.append(concat_vec, sen_matrix[:, i])          
-        # Padding with vector of zeros on the right
-        if I + W >= L:
-            concat_vec = np.append(concat_vec, np.zeros(abs(I+W+1-L) * D))
-
-        vectors[I,:] = concat_vec
-        
-    return vectors
+#     return vectors
 
 
-def mean_vectors(sentence, D, W, w2v_model):
-    '''
-    sentence: list of strings
-    D: dimensionality of word vectors
-    W: window
-    w2v_model: word2vec model
+# def mean_vectors(sentence, D, W, w2v_model):
+#     '''
+#     sentence: list of strings
+#     D: dimensionality of word vectors
+#     W: window
+#     w2v_model: word2vec model
     
-    return vectors: L x D matrix, where L is the length of the sentence.
-    i.e. each row contains a word vector after apply mean strategy.
-    '''
-    L = len(sentence)
-    shape = (D, L)    
-    sen_matrix = np.zeros(shape)
-    vectors = np.zeros((L, D))
+#     return vectors: L x D matrix, where L is the length of the sentence.
+#     i.e. each row contains a word vector after apply mean strategy.
+#     '''
+#     L = len(sentence)
+#     shape = (D, L)    
+#     sen_matrix = np.zeros(shape)
+#     vectors = np.zeros((L, D))
     
-    sen_matrix = fill_w2vMatrix(sen_matrix, w2v_model)
-        
-    for I in range(L):
-        vectors[I,:] = np.mean([sen_matrix[:,i] for i in range(I - W, I + W + 1) 
-                                if i >= 0 and i != I and i < L], axis=0)       
-    return vectors
+#     sen_matrix, count = fill_w2vMatrix(sentence, sen_matrix, D, w2v_model)
+    
+#     for I in range(L):
+#         vectors[I,:] = np.mean([sen_matrix[:,i] for i in range(I - W, I + W + 1) 
+#                                 if i >= 0 and i != I and i < L], axis=0)       
+#     return vectors
 
 
-def fractional_decay(sentence, D, W, w2v_model):
-    '''
-    sentence: list of strings
-    D: dimensionality of word vectors
-    W: window
-    w2v_model: word2vec model
+# def fractional_decay(sentence, D, W, w2v_model):
+#     '''
+#     sentence: list of strings
+#     D: dimensionality of word vectors
+#     W: window
+#     w2v_model: word2vec model
     
-    return vectors: L x D matrix, where L is the length of the sentence.
-    i.e. each row contains a word vector after apply fractional decay strategy.
-    '''
-    L = len(sentence)
-    shape = (D, L)
-    sen_matrix = np.zeros(shape)
-    vectors = np.zeros((L, D))
+#     return vectors: L x D matrix, where L is the length of the sentence.
+#     i.e. each row contains a word vector after apply fractional decay strategy.
+#     '''
+#     L = len(sentence)
+#     shape = (D, L)
+#     sen_matrix = np.zeros(shape)
+#     vectors = np.zeros((L, D))
     
-    sen_matrix = fill_w2vMatrix(sen_matrix, w2v_model)
-        
-    for I in range(L):
-        vectors[I,:] = np.sum([sen_matrix[:,j] * ((W - abs(I-j)) / W) 
-                               for j in range(I - W, I + W + 1) 
-                                if j >= 0 and j != I and j < L], axis=0)        
-    return vectors
+#     sen_matrix, count = fill_w2vMatrix(sentence, sen_matrix, D, w2v_model)
+    
+#     for I in range(L):
+#         vectors[I,:] = np.sum([sen_matrix[:,j] * ((W - abs(I-j)) / W) 
+#                                for j in range(I - W, I + W + 1) 
+#                                 if j >= 0 and j != I and j < L], axis=0)        
+#     return vectors
 
 
 def exponential_decay(sentence, D, W, w2v_model):
@@ -114,7 +117,8 @@ def exponential_decay(sentence, D, W, w2v_model):
     sen_matrix = np.zeros(shape)
     vectors = np.zeros((L, D))
     
-    sen_matrix = fill_w2vMatrix(sen_matrix, w2v_model)    
+    sen_matrix, count, no_vocab_list = fill_w2vMatrix(sentence, sen_matrix, D, w2v_model)    
+    
     # Decay parameter alpha: We choose the parameter in such a way that the immediate words 
     # that surround the target word contribute 10 times more than the last words 
     # on both sides of the window.
@@ -124,4 +128,5 @@ def exponential_decay(sentence, D, W, w2v_model):
         vectors[I,:] = np.sum([sen_matrix[:,j] * ((1-alpha)**(abs(I-j)-1)) 
                                for j in range(I - W, I + W + 1) 
                                 if j >= 0 and j != I and j < L], axis=0)       
-    return vectors
+    # count: amount of words that are not in the vocabulary. They are transformed to zeros vectors.
+    return vectors, count, no_vocab_list
